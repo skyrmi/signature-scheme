0a183bf0b9eb3917d0c24a93dc20890f62f62a5f
halyris <81964336+halyris@users.noreply.github.com>
2024-08-06 19:13:08 +0530

    brute-force minimum distance construction

--- src/matrix.h
+++ src/matrix.h
@@ -3,8 +3,8 @@
 
 void print_matrix(int rows, int cols, int matrix[rows][cols], const char *name);
 void transpose_matrix(int rows, int cols, int matrix[rows][cols], int transpose[cols][rows]);
-void multiply_matrices_gf2(bool_mat_t C, bool_mat_t A, bool_mat_t B);
-void make_systematic(unsigned long n, unsigned long k, bool_mat_t H);
+void multiply_matrices_gf2(nmod_mat_t C, const nmod_mat_t A, const nmod_mat_t B);
+void make_systematic(unsigned long n, unsigned long k, nmod_mat_t H);
 void rref(int num_rows, int num_cols, int (*H)[num_cols]);
 
 #endif
